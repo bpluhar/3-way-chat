e013609e73740b29963937d6512a75fa8c280e7e
Brian Pluhar <pluharbrian@gmail.com>
2024-10-18 12:37:54 -0400

    Reverted IP Change

--- app/api/token-counts/route.ts
+++ app/api/token-counts/route.ts
@@ -5,7 +5,7 @@ export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export async function GET() {
-  const pb = new PocketBase('https://127.0.0.1:8090/');
+  const pb = new PocketBase('https://pocket.leaselogic.app/');
 
   try {
     const data = await pb.collection('token_counts').getOne('1234567890abcde');
